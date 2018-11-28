cfc84dc7961cef64783a8270c3f6d924e13809eb
Giannis Daras <daras.giannhs@gmail.com>
2018-11-28 16:24:08 +0200

    Fix bug

--- algorithms/school-exercises/exercises/ex2/tourist.cpp
+++ algorithms/school-exercises/exercises/ex2/tourist.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <limits.h>
 using namespace std;
 
 int main(void){
@@ -16,23 +17,27 @@ int main(void){
   }
 
   int DP[N][K][C+1];
-  // thewroume sortarismena anamnistika apo to fthinotero sto akrivotero
+
   for (int j=0; j<K; j++){
     for (int c=0; c<=C; c++){
-      if (c > values[0][j]){
+      if (c >= values[0][j]){
         DP[0][j][c] = emotions[0][j];
       }
       else{
-        DP[0][j][c] = 0;
+        DP[0][j][c] = DP[0][j-1][c];
       }
     }
   }
   for (int i=1; i<N; i++){
     for (int j=0; j<K; j++){
       for (int c=0; c<=C; c++){
+        // an eisai sto 0 kai exeis lefta parto kai des allo provlima
         if (j == 0 && c >= values[i][j]) DP[i][j][c] = emotions[i][j] + DP[i-1][K-1][c-values[i][j]];
-        else if (j==0 && c<values[i][j]) DP[i][j][c] = DP[i-1][K-1][c - values[i][j]];
+        // an eisai sto 0 kai den exeis lefta, eisai doomed case.
+        else if (j==0 && c<values[i][j]) DP[i][j][c] = INT_MIN;
+        // an den mporeis na to pareis, dokimase na pareis kapoio fthinotero.
         else if (c<values[i][j]) DP[i][j][c] = DP[i][j-1][c];
+        // an mporeis na to pareis, des an se simferei na to pareis.
         else DP[i][j][c] = max(DP[i-1][K-1][c - values[i][j]] + emotions[i][j], DP[i][j-1][c]);
 
       }
